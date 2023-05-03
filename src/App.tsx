be0f4eaf8e1b0aa390f95b9b2ad6f17e598b7aff
Guilherme <devguirf11@gmail.com>
2023-05-03 17:33:38 -0300

    alterando logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import * as C from './App.styles'
 
-import logoImage from './assets/devmemory_logo.png'
+import logoImage from './assets/logoKelvin.jpg'
 import RestartIcon from './svgs/restart.svg'
 
 import { Button } from './components/Button'
